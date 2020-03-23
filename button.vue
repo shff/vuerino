2c9ab85a0acf9c99802296eaac9340026fdff010
Silvio Henrique Ferreira <shferreira@me.com>
2020-03-23 21:44:23 +0000

    first commit

---
+++ button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button>
+    {{ label }}
+  </button>
+</template>
+
+<script>
+  export default {
+    name: "btn",
+    props: {
+      label: String
+    }
+  };
+</script>
+
+<style>
+body { background: #eee; }
+</style>
